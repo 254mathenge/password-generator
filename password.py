7877943e7e0d8714a1ec4c35e46dd9c810b7f581
254mathenge <mathengefaith49@gmail.com>
2024-09-28 22:57:16 +0300

    adds comments

--- password.py
+++ password.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-
+# Define the function to generate a random password
 def passwordStrong(length=10):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = "".join(random.choice(characters) for i in range(length))
@@ -10,10 +10,12 @@ def passwordStrong(length=10):
 
 generate_password = input("generate password? ").lower()
 if generate_password == "yes":
+    #the number of characters in the password
     number_of_passwords = input("type the number of passwords you want: ")
     print("okay lets start!")
+    #loops the number of time  the user wants
     for i in range(int(number_of_passwords)):
-        # for i in range(int(number_of_passwords)):
+        #calling the function to be executed
         passwordStrong()
 else:
     print("goodbye")
